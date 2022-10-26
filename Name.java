0c1bb5ee0985ffe1d276036d0a80a79d308d370b
Lavanya507 <115569355+Lavanya507@users.noreply.github.com>
2022-10-26 11:34:15 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,97 @@
+package assignment;
+
+public class Name {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==0||i==n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j=0;j<n;j++)
+			{
+				if(i==0&&j>0&&j<n-1||j==0&&i>0||j==n-1&&i>0||i==(n-1)/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j=0;j<=(n*2)-1;j++)
+			{
+				if(i-j==0||i+j==(n*2)-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j=0;j<n;j++)
+			{
+				if(i==0&&j>0&&j<n-1||j==0&&i>0||j==n-1&&i>0||i==(n-1)/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j=0;j<n;j++)
+			{
+				if(j==0||j==n-1||i==j)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}	
+			}
+			System.out.print(" ");
+			for(int j=0;j<=(n*2)-4;j++)
+			{
+				if(j==(3*n)/4&&i>(n-1)/2||i-j==0&&i<=(n-1)/2||i+j==(n*2)-4&&i<=(n-1)/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			for(int j=0;j<n;j++)
+			{
+				if(i==0&&j>0&&j<n-1||j==0&&i>0||j==n-1&&i>0||i==(n-1)/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
